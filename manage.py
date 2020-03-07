ae73dc2dd1d09b54ebd74a97e3f3deb068ee37f6
Vladimir Roncevic <vroncevic@debtux>
2020-03-07 22:42:58 +0100

    [manage_flask] pyqc and pydoc done, updated doc

--- manage.py
+++ manage.py
@@ -1,4 +1,24 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+
+"""
+ Module
+     manage.py
+ Copyright
+     Copyright (C) 2017 Vladimir Roncevic <elektron.ronca@gmail.com>
+     manage_flask is free software: you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the
+     Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+     manage_flask is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+     See the GNU General Public License for more details.
+     You should have received a copy of the GNU General Public License along
+     with this program. If not, see <http://www.gnu.org/licenses/>.
+ Info
+     Main entry point of tool manage_flask.
+"""
+
 __author__ = "Vladimir Roncevic"
 __copyright__ = "Copyright 2017, Free software to use and distributed it."
 __credits__ = ["Vladimir Roncevic"]
@@ -8,39 +28,36 @@ __maintainer__ = "Vladimir Roncevic"
 __email__ = "elektron.ronca@gmail.com"
 __status__ = "Updated"
 
-import coverage
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-from manage_commands.create_database import CreateDatabase
-from manage_commands.drop_database import DropDatabase
-from manage_commands.create_data import CreateData
-from manage_commands.create_superuser import CreateSuperUser
-from manage_commands.run_coverage import RunCoverage
-from manage_commands.run_test import RunTest
+import sys
 
-COV = coverage.coverage(
-	branch=True,
-	include="app_server/*",
-	omit=[
-		"app_server/tests/*",
-		"app_server/configuration/testing_config.py",
-		"app_server/*/__init__.py"
-	]
-)
+try:
+    import coverage
 
-COV.start()
+    from flask_migrate import Migrate, MigrateCommand
+    from flask_script import Manager
 
-from app_server import app, db
+    from manage_commands.create_database import CreateDatabase
+    from manage_commands.drop_database import DropDatabase
+    from manage_commands.create_data import CreateData
+    from manage_commands.create_superuser import CreateSuperUser
+    from manage_commands.run_coverage import RunCoverage
+    from manage_commands.run_test import RunTest
 
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command("db", MigrateCommand)
-manager.add_command("create_db", CreateDatabase(db))
-manager.add_command("drop_db", DropDatabase(db))
-manager.add_command("create_data", CreateData(db))
-manager.add_command("createsuperuser", CreateSuperUser(db))
-manager.add_command("test", RunTest())
-manager.add_command("coverage", RunCoverage(COV))
+    COV = coverage.coverage(
+        branch=True,
+        include="app_server/*",
+        omit=[
+            "app_server/tests/*",
+            "app_server/configuration/testing_config.py",
+            "app_server/*/__init__.py"
+        ]
+    )
+    COV.start()
+
+    from app_server import app, db
+except ImportError as error:
+    MESSAGE = "\n{0}\n{1}\n".format(__file__, error)
+    sys.exit(MESSAGE)  # Force close python ATS ##############################
 
 # python manage.py create_db
 # python manage.py db init
@@ -50,4 +67,13 @@ manager.add_command("coverage", RunCoverage(COV))
 # python manage.py runserver
 
 if __name__ == "__main__":
-	manager.run()
+    MIGRATE = Migrate(app, db)
+    MANAGER = Manager(app)
+    MANAGER.add_command("db", MigrateCommand)
+    MANAGER.add_command("create_db", CreateDatabase(db))
+    MANAGER.add_command("drop_db", DropDatabase(db))
+    MANAGER.add_command("create_data", CreateData(db))
+    MANAGER.add_command("createsuperuser", CreateSuperUser(db))
+    MANAGER.add_command("test", RunTest())
+    MANAGER.add_command("coverage", RunCoverage(COV))
+    MANAGER.run()
